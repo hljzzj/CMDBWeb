625c8a73e20439a775f60bf6768780702b21159e
hnga <hljzzj@qq.com>
2016-05-27 18:04:27 +0800

    Initial commit

---
+++ WebRoot/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+# Create your views here.
+
+
+def index(request,kwargs):
+    return HttpResponse('index.html')
+
+def index(request):
+    return render(request,'index.html',locals())
